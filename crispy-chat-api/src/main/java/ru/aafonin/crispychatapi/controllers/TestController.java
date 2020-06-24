22285602b906a418abc61af9fb5969afaf2b0385
aafonin <aafonin@tsconsulting.ru>
2020-06-24 23:21:28 +0300

    add slf4j

--- crispy-chat-api/src/main/java/ru/aafonin/crispychatapi/controllers/TestController.java
+++ crispy-chat-api/src/main/java/ru/aafonin/crispychatapi/controllers/TestController.java
@@ -1,5 +1,6 @@
 package ru.aafonin.crispychatapi.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import ru.aafonin.crispychatapi.repositories.UserRepository;
 
 import java.util.List;
 
+@Slf4j
 @RestController()
 public class TestController {
     @Autowired
@@ -18,6 +20,7 @@ public class TestController {
 
     @GetMapping("/test/123")
     public List<User> test() {
+        log.info("call test() method");
         User user = new User();
         user.setUsername("user1");
         user.setPassword("password2");
